bc2a840a6c0c6d991714e4e7a80be14574e2d108
Sandra Cabanes <sandraxtv@gmail.com>
2019-01-22 08:17:09 +0100

    Inicio

---
+++ src/model/Alumno.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Sandra
+ */
+public class Alumno implements Serializable {
+    private int id;
+    private String nombre;
+    private float sueldo;
+    private boolean registrado;
+    private Date fechaNac;
+    private Date horaTutoria;
+    private Date fechaRegistro;
+    private String observaciones;
+
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, float sueldo, boolean registrado, Date fechaNac, Date horaTutoria, Date fechaRegistro, String observaciones) {
+        this.id = id;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.registrado = registrado;
+        this.fechaNac = fechaNac;
+        this.horaTutoria = horaTutoria;
+        this.fechaRegistro = fechaRegistro;
+        this.observaciones = observaciones;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isRegistrado() {
+        return registrado;
+    }
+
+    public void setRegistrado(boolean registrado) {
+        this.registrado = registrado;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public Date getHoraTutoria() {
+        return horaTutoria;
+    }
+
+    public void setHoraTutoria(Date horaTutoria) {
+        this.horaTutoria = horaTutoria;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "id=" + id + ", nombre=" + nombre + ", sueldo=" + sueldo + ", registrado=" + registrado + ", fechaNac=" + fechaNac + ", horaTutoria=" + horaTutoria + ", fechaRegistro=" + fechaRegistro + ", observaciones=" + observaciones + '}';
+    }
+    
+    
+}
